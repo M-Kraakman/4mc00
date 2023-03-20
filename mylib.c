842fc5d68afbed1d5ad1fe5c7104a6ed28b3e9f9
Mattias Hart <m.d.e.hart@student.tue.nl>
2023-03-20 11:05:13 +0100

    Aanpassinkjes

--- mylib.c
+++ mylib.c
@@ -172,7 +172,7 @@ void solve
 
   }
 
-  double a = 0., b = 0.;
+  double a = 1., b = 0.;
   double kp = model->s[0].kp;
   double ks = model->s[0].ke;
 
@@ -187,8 +187,8 @@ void solve
 
     model->s[iSpr].uij = model->s[iSpr].length - model->s[iSpr].length0;
 
-    model->f[iSpr].fs.x = ks * model->s[iSpr].uij * -a;
-    model->f[iSpr].fs.y = ks * model->s[iSpr].uij * 1.;
+    model->f[iSpr].fs.x = ks * model->s[iSpr].uij;
+    model->f[iSpr].fs.y = ks * model->s[iSpr].uij;
 
     model->p[model->s[iSpr].p1 -1].f.x += model->f[iSpr].fs.x;
     model->p[model->s[iSpr].p1 -1].f.y += model->f[iSpr].fs.y;
@@ -226,11 +226,11 @@ void solve
         model->p[iPar].f.x += model->f[iPar].fc.x;
         model->p[iPar].f.y += model->f[iPar].fc.y;
     }
-    else
+    /*else
     {
         model->p[iPar].f.x += 0.;
         model->p[iPar].f.y += 0.;
-    }
+    }*/
 
     /*printf("%.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f \n",
 
@@ -260,7 +260,7 @@ void solve
     model->p[iPar].v.x += 0.5*DT*model->p[iPar].a.x;
     model->p[iPar].v.y += 0.5*DT*model->p[iPar].a.y;
 
-    /*printf("%2f %2f\n", model->p[iPar].f.x, model->p[iPar].f.y);*/   
+    printf("%2f %2f\n", model->p[iPar].f.x, model->p[iPar].f.y);   
   }
 }
 
